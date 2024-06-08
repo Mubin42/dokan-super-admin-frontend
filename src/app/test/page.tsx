f26e889ef6afbcfa4be69dca1a21a614dcaed7c2
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2024-06-08 20:52:33 +0600

    first commit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,30 @@
+import PageLayout from '@/components/layouts/page-layout/PageLayout';
+import React, { FC } from 'react';
+
+type TestProps = {};
+
+const Test: FC<TestProps> = ({}) => {
+	// api
+
+	// hooks
+
+	// states
+
+	// variables
+
+	// styles
+
+	// functions
+
+	// effects
+
+	// components
+
+	return (
+		<PageLayout>
+			<div className='bg-blue-300 h-full'>A</div>
+		</PageLayout>
+	);
+};
+
+export default Test;
---
+++ src/components/layouts/page-layout/PageLayout.tsx
@@ -0,0 +1,38 @@
+import { cn } from '@/lib/utils';
+import React, { FC } from 'react';
+import Sidebar from '../sidebar/Sidebar';
+
+type PageLayoutProps = {
+	className?: string;
+	children: React.ReactNode;
+};
+
+const PageLayout: FC<PageLayoutProps> = ({ className, children }) => {
+	// api
+
+	// hooks
+
+	// states
+
+	// variables
+
+	// styles
+
+	// functions
+
+	// effects
+
+	// components
+
+	return (
+		<div className={cn('h-screen w-screen flex', className)}>
+			<Sidebar />
+			<div className='flex flex-col w-full'>
+				<div className='w-full h-16 bg-red-300'></div>
+				<main className='flex-1'>{children}</main>
+			</div>
+		</div>
+	);
+};
+
+export default PageLayout;
