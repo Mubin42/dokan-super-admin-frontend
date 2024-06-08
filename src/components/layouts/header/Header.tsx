df1fadeaf6dbf79ac71b71a4e02a023e23ccf5de
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2024-06-08 23:25:45 +0600

    layout fix for the product

--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -1,4 +1,5 @@
 import AccountDropdown from '@/components/sections/account-dropdown/AccountDropdown';
+import { ModeToggle } from '@/components/sections/mode-toggle/ModeToggle';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Input } from '@/components/ui/input';
 import { Search } from 'lucide-react';
@@ -33,7 +34,7 @@ const Header: FC<HeaderProps> = ({}) => {
 	);
 
 	return (
-		<header className='flex h-16 w-full items-center gap-4 border-b bg-slate-100 px-4'>
+		<header className='flex h-16 w-full items-center gap-4 border-b bg-background px-4'>
 			<h1 className='text-xl font-bold'>Dokan Admin App</h1>
 			<div className='relative ml-auto flex-1 md:grow-0'>
 				<Search className='absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground' />
@@ -43,6 +44,7 @@ const Header: FC<HeaderProps> = ({}) => {
 					className='w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]'
 				/>
 			</div>
+			<ModeToggle />
 			{user}
 		</header>
 	);
